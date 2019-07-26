c110ee926e7f68973e591a00b2d3db58cea35e96
ddmilburn02 <45765956+ddmilburn02@users.noreply.github.com>
2019-07-26 02:30:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const { Client, RichEmbed } = require('discord.js');
 const client = new Client();
-const config = require('config.json')
+const config = require('./config.json')
 
 client.on('ready', () => {
   console.log('I am ready!');
+  client.user.setActivity('the VG Discord Server.', { type: 'WATCHING' });
 });
 
 client.on('message', message => {
-  if (message.content === 'how to embed') {
+/**  if (message.content === 'how to embed') {
     const embed = new RichEmbed()
       .setTitle('A slick little embed')
       .setColor(0xFF0000)
       .setDescription('Hello, this is a slick embed!');
     message.channel.send(embed);
   }
+*/
+    if (message.content === config.h1) {
+      message.delete(1);
+      message.channel.send(config.h1a)
+    }
 });
 
 client.login(config.token);
